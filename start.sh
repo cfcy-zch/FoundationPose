0c76741bf301d4fb6113bb2d2e58584804894da3
cfcy-zch <h2812703468@gmail.com>
2024-05-20 15:04:47 +0800

    .sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
-source /opt/ros/melodic/setup.bash
-source ~/robot_ws/devel/setup.bash
-gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch zoo_robot robot_lidar.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch zoo_control keyboard_teleop.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch robot_slam gmapping.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch robot_slam view_mapping.launch; exec bash"' \
+source ~/ros1_ws/devel/setup.bash
+gnome-terminal --window -- bash -c "roslaunch aubo_i5_moveit_config moveit_planning_execution.launch robot_ip:=127.0.0.1; exec bash"  \
+--tab -- -- bash -c "sleep 5; source ~/anaconda3/etc/profile.d/conda.sh; conda activate foundationpose; /home/hh/anaconda3/envs/foundationpose/bin/python /home/hh/ros1_ws/src/FoundationPose/grasp_final.py; exec bash"  \
+# gnome-terminal --window -- bash -c "sleep 5; source ~/anaconda3/etc/profile.d/conda.sh; conda activate foundationpose; /home/hh/anaconda3/envs/foundationpose/bin/python /home/hh/ros1_ws/src/FoundationPose/grasp_final.py; exec bash"  \
+# --tab -- bash -c "sleep 5; roslaunch aubo_i5_moveit_config moveit_planning_execution.launch robot_ip:=127.0.0.1; exec bash" \
+# --tab -e 'bash -c "sleep 5; roslaunch zoo_control keyboard_teleop.launch; exec bash"' \
+# --tab -e 'bash -c "sleep 5; roslaunch robot_slam gmapping.launch; exec bash"' \
+# --tab -e 'bash -c "sleep 5; roslaunch robot_slam view_mapping.launch; exec bash"' \
 
 
